da2d1b4fbae52978d063f1fb5d6b73b0e7e4060f
Hritik Arora <hritikarora875@gmail.com>
2021-07-07 22:11:49 +0530

    contact form updated

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.enums import IntegerChoices
 
 # Create your models here.
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True);
+    name = models.CharField(max_length=100);
+    phone = models.IntegerField(max_length=13);
+    email = models.EmailField(max_length=50)
+    content = models.TextField();
+    def __str__(self) :
+        return 'Message from'+self.name
+
+
+   
+
+  
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from .models import Contact
 
 # Create your views here.
 
@@ -8,4 +10,17 @@ def home(request):
 def about(request):
     return render(request,"about.html")
 def contact(request):
+    if request.method =='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        msg = request.POST['msg']
+        print(f'{name},{email},{phone},{msg}')
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(msg)<4:
+            messages.error(request,"Please fill the form correctly")
+        else:
+            messages.success(request,"your call was sucessful")
+            contact = Contact(name=name, email=email,phone=phone, content=msg)
+            contact.save()
+
     return render(request,"contact.html")
